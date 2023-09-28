15d3d5340f0f2027082cea4d1812eb7cb76e40ad
digital <132694082+digtail@users.noreply.github.com>
2023-09-28 15:06:40 +0200

    nixos/boot/generic-extlinux: implement support for boot.initrd.secrets

--- nixos/modules/system/boot/loader/generic-extlinux-compatible/extlinux-conf-builder.nix
+++ nixos/modules/system/boot/loader/generic-extlinux-compatible/extlinux-conf-builder.nix
@@ -1,10 +1,14 @@
-{ lib, pkgs }:
+{ config, lib, pkgs }:
 
 pkgs.replaceVarsWith {
   src = ./extlinux-conf-builder.sh;
   isExecutable = true;
   replacements = {
     path = lib.makeBinPath [pkgs.coreutils pkgs.gnused pkgs.gnugrep];
+    doSecrets =
+      if config.boot.loader.supportsInitrdSecrets
+      then "true"
+      else "false";
     inherit (pkgs) bash;
   };
 }
